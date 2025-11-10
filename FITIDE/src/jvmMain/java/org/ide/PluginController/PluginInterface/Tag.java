619d9acae18d5ce197d82dfc22bc7c7c64b95628
system1cls <d.faranosov@gmail.com>
2025-11-10 17:57:28 +0700

    New plugin reqs

--- FITIDE/src/jvmMain/java/org/ide/PluginController/PluginInterface/Tag.java
+++ FITIDE/src/jvmMain/java/org/ide/PluginController/PluginInterface/Tag.java
@@ -1,4 +1,17 @@
 package org.ide.PluginController.PluginInterface;
 
 public enum Tag {
+    Class,
+    Statement,
+    ImportStatement,
+    Func,
+    Var,
+    Constant,
+    KeyWord,
+    If,
+    While,
+    For,
+    Declaration,
+    Definition,
+    Error
 }
