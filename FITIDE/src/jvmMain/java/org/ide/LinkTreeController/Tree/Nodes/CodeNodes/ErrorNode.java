abc501450699e7b103508f8c7bfa967d9792a74a
system11cls <d.faranosov@g.nsu.ru>
2025-12-02 12:58:35 +0700

    ErrorNode added

--- FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/Nodes/CodeNodes/ErrorNode.java
+++ FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/Nodes/CodeNodes/ErrorNode.java
@@ -3,11 +3,16 @@ package org.ide.LinkTreeController.Tree.Nodes.CodeNodes;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.ide.LinkTreeController.Tree.Nodes.Abstract.AInternalCodeNode;
 import org.ide.LinkTreeController.Tree.ToolClasses.LinkTreePosition;
+import org.ide.PluginController.PluginInterface.Plugin;
 
 import java.nio.file.Path;
 import java.util.List;
 
 public class ErrorNode extends AInternalCodeNode {
+    public ErrorNode(Plugin plugin, Path pathToFile, Path path, ParseTree tree, String name) {
+        super(plugin, pathToFile, path, tree, name);
+    }
+
     @Override
     protected void setChilds(ParseTree curNode) {
     }
