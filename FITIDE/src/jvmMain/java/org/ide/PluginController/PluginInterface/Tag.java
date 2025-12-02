7f99b5505e95c37cd79f37d636a5cee7b1459a47
system11cls <d.faranosov@g.nsu.ru>
2025-12-02 12:34:50 +0700

    updated reqs

--- FITIDE/src/jvmMain/java/org/ide/PluginController/PluginInterface/Tag.java
+++ FITIDE/src/jvmMain/java/org/ide/PluginController/PluginInterface/Tag.java
@@ -2,13 +2,9 @@ package org.ide.PluginController.PluginInterface;
 
 public enum Tag {
     Class,
-    Type,
-    Statement,
     ImportStatement,
     Func,
     Var,
-    Constant,
-    KeyWord,
     Construction,
     Declaration,
     Definition,
