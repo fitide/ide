83ffb7b98c589867a600c491eca50d5d74a14f8a
system1cls <d.faranosov@gmail.com>
2025-12-09 16:36:33 +0700

    start redo Construction to list of bodies

--- FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/Nodes/CodeNodes/Construction.java
+++ FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/Nodes/CodeNodes/Construction.java
@@ -1,5 +1,6 @@
 package org.ide.LinkTreeController.Tree.Nodes.CodeNodes;
 
+import org.antlr.v4.runtime.misc.Pair;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.ide.LinkTreeController.Tree.Nodes.Abstract.AInternalCodeNode;
 import org.ide.LinkTreeController.Tree.Nodes.Abstract.CodeType;
@@ -18,8 +19,10 @@ public class Construction extends AInternalCodeNode {
     public Map<String, AInternalCodeNode> args = new HashMap<>();
     public LinkTreePosition bodyPosition;
     public LinkTreePosition argsPosition;
+    public List<Pair<Map<String, AInternalCodeNode>, LinkTreePosition>> bodies;
 
     public Construction(Plugin plugin, Path pathToFile, Path path, ParseTree tree) {
+        //TODO: override
         super(plugin, pathToFile, path, tree);
         if (codeType == CodeType.Error) return;
 
@@ -38,6 +41,8 @@ public class Construction extends AInternalCodeNode {
 
     @Override
     protected void setChilds(ParseTree curNode) {
+        //TODO: override
+
         this.childs = TreeBuilder.getChilds(plugin, curNode, pathToFile, pathToModule);
         List<ParseTree> argsTrees = plugin.getArgsOfFunc(curNode);
         for (ParseTree tree : argsTrees) {
@@ -67,9 +72,14 @@ public class Construction extends AInternalCodeNode {
 
     @Override
     public void getHighlightning(List<CodeStrForColour> list) {
-        super.getHighlightning(list);
         for (AInternalCodeNode arg : args.values()) {
             arg.getHighlightning(list);
+        }
+
+        for (var body : bodies) {
+            for (var state : body.a.values()) {
+                state.getHighlightning(list);
+            }
         }
     }
 
@@ -82,11 +92,12 @@ public class Construction extends AInternalCodeNode {
                     return Paths.get(pathToNode.toString(), node.id);
                 }
             }
-
-            for (AInternalCodeNode node : childs.values()) {
-                if (node.name != null && node.name.equals(name) &&
-                        (node.codeType == CodeType.Declaration || node.codeType == CodeType.Definition)) {
-                    return Paths.get(pathToNode.toString(), node.id);
+            for (var body : bodies) {
+                for (var  node : body.a.values()) {
+                    if (node.name != null && node.name.equals(name) &&
+                            (node.codeType == CodeType.Declaration || node.codeType == CodeType.Definition)) {
+                        return Paths.get(pathToNode.toString(), node.id);
+                    }
                 }
             }
         }
@@ -95,8 +106,10 @@ public class Construction extends AInternalCodeNode {
                 return args.get(PathTools.getRootStr(pathToModule)).searchForDeclaration(PathTools.deleteRoot(pathToModule), name);
             }
 
-            if (childs.containsKey(PathTools.getRootStr(pathToModule))) {
-                return childs.get(PathTools.getRootStr(pathToModule)).searchForDeclaration(PathTools.deleteRoot(pathToModule), name);
+            for (var body : bodies) {
+                if (body.a.containsKey(PathTools.getRootStr(pathToModule))) {
+                    return childs.get(PathTools.getRootStr(pathToModule)).searchForDeclaration(PathTools.deleteRoot(pathToModule), name);
+                }
             }
         }
 
@@ -112,19 +125,23 @@ public class Construction extends AInternalCodeNode {
                 }
             }
 
-            for (AInternalCodeNode node : childs.values()) {
-                if (node.name != null && node.name.equals(name) && node.codeType == CodeType.Definition) {
-                    return Paths.get(pathToNode.toString(), node.id);
+            for (var body : bodies) {
+                for (var  node : body.a.values()) {
+                    if (node.name != null && node.name.equals(name) && node.codeType == CodeType.Definition) {
+                        return Paths.get(pathToNode.toString(), node.id);
+                    }
                 }
             }
         }
         else {
             if (args.containsKey(PathTools.getRootStr(pathToModule))) {
-                return args.get(PathTools.getRootStr(pathToModule)).searchForDeclaration(PathTools.deleteRoot(pathToModule), name);
+                return args.get(PathTools.getRootStr(pathToModule)).searchForDefinition(PathTools.deleteRoot(pathToModule), name);
             }
 
-            if (childs.containsKey(PathTools.getRootStr(pathToModule))) {
-                return childs.get(PathTools.getRootStr(pathToModule)).searchForDeclaration(PathTools.deleteRoot(pathToModule), name);
+            for (var body : bodies) {
+                if (body.a.containsKey(PathTools.getRootStr(pathToModule))) {
+                    return childs.get(PathTools.getRootStr(pathToModule)).searchForDefinition(PathTools.deleteRoot(pathToModule), name);
+                }
             }
         }
 
@@ -153,6 +170,8 @@ public class Construction extends AInternalCodeNode {
 
     @Override
     protected void updateTree(ParseTree tree) {
+        //TODO: override
+
         AInternalCodeNode node = TreeBuilder.buildOneChild(plugin, tree, pathToFile, PathTools.deleteLast(pathToModule));
         if (node instanceof Construction) {
             this.updateCurNode(node);
@@ -166,16 +185,18 @@ public class Construction extends AInternalCodeNode {
 
     @Override
     protected List<Path> getPaths(LinkTreePosition position) {
-        if (namePosition.compareTo(position) == 0) return List.of();
-        if (bodyPosition.compareTo(position) == 0) {
-            List<Path> res = new ArrayList<>();
+        for (var body : bodies) {
+            if (body.b.compareTo(position) == 0) {
+                List<Path> res = new ArrayList<>();
 
-            for (AInternalCodeNode node : childs.values()) {
-                if (node.wholePos.compareTo(position) == 0) res = node.getPathsToSearchDeclaration(position);
+                for (AInternalCodeNode node : body.a.values()) {
+                    if (node.wholePos.compareTo(position) == 0) res = node.getPathsToSearchDeclaration(position);
+                }
+
+
+                res.add(this.pathToModule);
+                return res;
             }
-
-            res.add(this.pathToModule);
-            return res;
         }
 
         if (argsPosition.compareTo(position) == 0) {
@@ -211,16 +232,22 @@ public class Construction extends AInternalCodeNode {
             arg.setDefinitionsAndDeclarations(defs, decs);
         }
 
-        for(var node : this.childs.values()) {
-            node.setDefinitionsAndDeclarations(defs, decs);
+        for (var body : bodies) {
+            for (var node : body.a.values()) {
+                node.setDefinitionsAndDeclarations(defs, decs);
+            }
         }
     }
 
     @Override
     public void setTypes(Set<String> types) {
-        super.setTypes(types);
         for (var arg : args.values()) {
             arg.setTypes(types);
+        }
+        for (var body : bodies) {
+            for (var node : body.a.values()) {
+                node.setTypes(types);
+            }
         }
     }
 }
