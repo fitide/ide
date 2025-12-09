b016f38f10eeba82bb6f28cab299d274ee996ed4
system1cls <d.faranosov@gmail.com>
2025-12-09 23:39:18 +0700

    work

--- FITIDE/src/jvmTest/java/org/ide/LinkTreeController/cdmTest/LinkTreeControllerCdmTest.java
+++ FITIDE/src/jvmTest/java/org/ide/LinkTreeController/cdmTest/LinkTreeControllerCdmTest.java
@@ -6,9 +6,12 @@ import org.ide.FileExplorerController.Exceptions.UnnableToCreateFileException;
 import org.ide.FileExplorerController.FileExplorerController;
 import org.ide.LinkTreeController.LinkTreeController;
 import org.ide.LinkTreeController.LinkTreeControllerImpl;
+import org.ide.LinkTreeController.Tree.Nodes.CodeNodes.Func;
+import org.ide.LinkTreeController.Tree.Nodes.FileNodes.CommonFile;
 import org.ide.LinkTreeController.cdmTest.cdm.CdmPlugin;
 import org.ide.LinkTreeController.cdmTest.cdm.CdmPluginTest;
 import org.ide.PluginController.PluginInterface.Plugin;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -43,5 +46,23 @@ class LinkTreeControllerCdmTest {
                 "C:\\Users\\user\\IdeaProjects\\ide\\FITIDE\\src\\jvmTest\\java\\org\\ide" +
                         "\\LinkTreeController\\cdmTest\\cdm\\testFiles\\simple.asm"));
         controller.updateTree(plugin, List.of(new Pair<>(Paths.get("testFiles", "simple.asm"), tree)));
+        var syn = controller.getSyntaxHighlightning(Paths.get("testFiles", "simple.asm"));
+        Assert.assertEquals(syn.size(), 4);
+    }
+
+    @Test
+    void simpleDec() {
+        controller.initFiles(plugin, List.of());
+        ParseTree tree = plugin.getFileParseTree(new File(
+                "C:\\Users\\user\\IdeaProjects\\ide\\FITIDE\\src\\jvmTest\\java\\org\\ide" +
+                        "\\LinkTreeController\\cdmTest\\cdm\\testFiles\\simple.asm"));
+        controller.updateTree(plugin, List.of(new Pair<>(Paths.get("testFiles", "simple.asm"), tree)));
+        var linkTree = controller.getTree();
+        var file = (CommonFile) linkTree.getFileNode(Paths.get("testFiles", "simple.asm"));
+        for (var node : file.codeNodes.values()) {
+            if (node instanceof Func) {
+                Assert.assertEquals(node.declaration, linkTree.getExternalFunctions().get("add"));
+            }
+        }
     }
 }
