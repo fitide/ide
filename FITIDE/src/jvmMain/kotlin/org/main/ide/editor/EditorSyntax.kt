ac80e656338e16a886c8eb9c1d63ccc895299993
ttomilov <t.tomilov@g.nsu.ru>
2025-12-16 13:05:42 +0700

    update syntax colors

--- FITIDE/src/jvmMain/kotlin/org/main/ide/editor/EditorSyntax.kt
+++ FITIDE/src/jvmMain/kotlin/org/main/ide/editor/EditorSyntax.kt
@@ -6,11 +6,11 @@ import org.main.ide.uistate.UIColors
 
 internal fun colorForTag(tag: LinkTreeCodeTag): Color =
     when (tag) {
-        LinkTreeCodeTag.KeyWord         -> Color(0xFF569CD6)
-        LinkTreeCodeTag.Var             -> Color(0xFF9CDCFE)
-        LinkTreeCodeTag.Func            -> Color(0xFFDCDCAA)
-        LinkTreeCodeTag.importStatement -> Color(0xFF4EC9B0)
-        LinkTreeCodeTag.constant    -> Color(0xFFC586C0)
-        LinkTreeCodeTag.Error       -> Color(0xFFFF5555)
+        LinkTreeCodeTag.KeyWord         -> Color(0xFFcc7732)
+        LinkTreeCodeTag.Var             -> Color(0xFFA9B7C6)
+        LinkTreeCodeTag.Func            -> Color(0xFF4A9EFF)
+        LinkTreeCodeTag.importStatement -> Color(0xFFCC7832)
+        LinkTreeCodeTag.constant    -> Color(0xFF9876AA)
+        LinkTreeCodeTag.Error       -> Color(0xFFBC3F3C)
         else               -> UIColors.TextPrimary
     }
