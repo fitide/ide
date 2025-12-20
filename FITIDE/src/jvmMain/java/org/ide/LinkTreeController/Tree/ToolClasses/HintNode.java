7f67724cb3dfc803fc3563c826a291b7ee3eab88
system1cls <d.faranosov@gmail.com>
2025-12-20 14:30:20 +0700

    fix

--- FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/ToolClasses/HintNode.java
+++ FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/ToolClasses/HintNode.java
@@ -3,6 +3,8 @@ package org.ide.LinkTreeController.Tree.ToolClasses;
 import org.ide.LinkTreeController.Tree.Nodes.Abstract.LinkTreeCodeTag;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class HintNode implements Comparable<HintNode> {
     public LinkTreeCodeTag type;
     public String name;
@@ -16,5 +18,18 @@ public class HintNode implements Comparable<HintNode> {
     @Override
     public int compareTo(@NotNull HintNode o) {
         return name.compareTo(o.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof HintNode node)) return false;
+
+        return node.name.equals(this.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
