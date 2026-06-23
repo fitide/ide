d128de4bb684146833b4cbc182ff3ec080c5e9e1
system1cls <d.faranosov@gmail.com>
2026-06-23 14:29:45 +0700

    sm fx

--- FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/Nodes/CodeNodes/ClassNode.java
+++ FITIDE/src/jvmMain/java/org/ide/LinkTreeController/Tree/Nodes/CodeNodes/ClassNode.java
@@ -141,6 +141,9 @@ public class ClassNode extends AInternalCodeNode {
             }
             default -> {}
         }
+
+
+        for (var node : childs.values()) node.addDefinitionsAndDeclarations(defs, decs);
     }
 
     @Override
